7cc4a12b6d785852324cf0a14024d5e69c0d8b9b
Marcin Wiatr <marcin@qi-interactive.com>
2015-02-01 12:48:13 +0000

    added dummy unit tests
    Signed-off-by: Marcin Wiatr <marcin@qi-interactive.com>

---
+++ tests/unit/DemoTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace yiiunit;
+use Yii;
+/**
+ * This is the base class for all yii framework unit tests, which requires
+ * external vendor libraries to function.
+ */
+class VendorTestCase extends \PHPUnit_Framework_TestCase {
+
+
+public function testCheck() {
+
+	$this->assertTrue(true);
+}
+
+
+}
