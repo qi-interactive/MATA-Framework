8d12543f78fb234eeb33dd51acc354e1e714e12e
Marcin Wiatr <marcin@qi-interactive.com>
2015-02-19 16:21:17 +0000

    added new function to ComposerHelper
    Signed-off-by: Marcin Wiatr <marcin@qi-interactive.com>

--- helpers/ComposerHelper.php
+++ helpers/ComposerHelper.php
@@ -13,4 +13,14 @@ class ComposerHelper {
 		if (array_key_exists($namespace, $includeFiles))
 			return current($includeFiles[$namespace]);
 	}
+
+	public static function getLibraryNamespaceByFolder($folder) {
+		$includeFiles = require(\Yii::getAlias('@vendor') . DIRECTORY_SEPARATOR . "composer"
+			. DIRECTORY_SEPARATOR . "autoload_psr4.php");
+
+		foreach ($includeFiles as $namespace => $value) {
+			if (current($value) == $folder)
+				return $namespace;
+		}
+	}
 }
