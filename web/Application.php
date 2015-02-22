4683fea4a73f8862645692d0060047ff7faeaa64
Marcin Wiatr <marcin@qi-interactive.com>
2015-02-22 11:00:04 +0000

    added MATA Application
    Signed-off-by: Marcin Wiatr <marcin@qi-interactive.com>

---
+++ web/Application.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace mata\web;
+
+use yii\web\Application as BaseApplication;
+
+class Application extends BaseApplication {
+
+	protected function bootstrap() {
+	    $request = $this->getRequest();
+	    parent::bootstrap();
+
+	    \Yii::setAlias('@webroot', dirname($request->getScriptFile())  . DIRECTORY_SEPARATOR . "web");
+	    \Yii::setAlias('@web', $request->getBaseUrl() . "/web");
+	}
+}
