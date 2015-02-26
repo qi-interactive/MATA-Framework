199cdb72d10d5b2fa36fd096204a8fc0e83d74b9
Michal Fiedorowicz <michal@qi-interactive.com>
2015-02-26 14:12:48 +0000

    DynamicForm added

---
+++ helpers/MataDynamicModelHelper.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace mata\helpers;
+
+use Yii;
+use mata\base\DynamicModel;
+use yii\db\Schema;
+
+class MataDynamicModelHelper {
+
+	public static function generateFromTableName($tableName, $omitId = true) {
+		if(!empty($tableName)) {
+    		$tableSchema = self::getDbConnection()->getTableSchema($tableName);
+    		if(!empty($tableSchema->columns)) {
+                $fields = [];
+                foreach ($tableSchema->columns as $column) {
+                    if($column->name == 'Id' && $omitId)
+                        continue;
+                    $fields[] = $column->name;
+                }
+                $dynamicModel = new DynamicModel($fields);
+                foreach($fields as $attribute) {
+                	$dynamicModel->setAttributeLabel($attribute, $attribute);
+                }
+                $dynamicModel->setTableName($tableName);
+
+                $dynamicModel = self::generateRulesFromTable($dynamicModel, $tableSchema, $omitId);
+
+                return $dynamicModel;
+            }
+        }
+        return null;
+	}
+
+	protected static function getDbConnection() {
+        return Yii::$app->getDb();
+    }
+
+    /**
+     * Generates validation rules for the specified table.
+     * @param \yii\db\TableSchema $table the table schema
+     * @return array the generated validation rules
+     */
+    protected static function generateRulesFromTable($dynamicModel, $tableSchema, $omitId = true)
+    {
+        $types = [];
+        $lengths = [];
+        foreach ($tableSchema->columns as $column) {
+            if($column->name == 'Id' && $omitId)
+                continue;
+            if ($column->autoIncrement) {
+                continue;
+            }
+            if (!$column->allowNull && $column->defaultValue !== null) {
+                $types['required'][] = $column->name;
+            }
+            switch ($column->type) {
+                case Schema::TYPE_SMALLINT:
+                case Schema::TYPE_INTEGER:
+                case Schema::TYPE_BIGINT:
+                    $types['integer'][] = $column->name;
+                    break;
+                case Schema::TYPE_BOOLEAN:
+                    $types['boolean'][] = $column->name;
+                    break;
+                case Schema::TYPE_FLOAT:
+                case Schema::TYPE_DECIMAL:
+                case Schema::TYPE_MONEY:
+                    $types['number'][] = $column->name;
+                    break;
+                case Schema::TYPE_DATE:
+                case Schema::TYPE_TIME:
+                case Schema::TYPE_DATETIME:
+                case Schema::TYPE_TIMESTAMP:
+                    $types['safe'][] = $column->name;
+                    $types['date'][] = $column->name;
+                    break;
+                default: // strings
+                    if ($column->size > 0) {
+                        $lengths[$column->size][] = $column->name;
+                    } else {
+                        $types['string'][] = $column->name;
+                    }
+                    if (stripos($column->name, 'email') !== false) {
+			            $types['email'][] = $column->name;
+			        } elseif (stripos($column->name, 'url') !== false) {
+			            $types['url'][] = $column->name;
+			        }
+            }
+        }
+        // $rules = [];
+        foreach ($types as $type => $columns) {
+            $dynamicModel->addRule($columns, $type);
+        }
+        foreach ($lengths as $length => $columns) {
+            $dynamicModel->addRule($columns, 'string', ['max' => $length]);
+        }
+
+        // Unique indexes rules
+        try {
+            $db = self::getDbConnection();
+            $uniqueIndexes = $db->getSchema()->findUniqueIndexes($tableSchema);
+            foreach ($uniqueIndexes as $uniqueColumns) {
+                // Avoid validating auto incremental columns
+                if (!$this->isColumnAutoIncremental($tableSchema, $uniqueColumns)) {
+                    $attributesCount = count($uniqueColumns);
+
+                    if ($attributesCount == 1) {
+                        $dynamicModel->addRule($uniqueColumns[0], 'unique');
+                    } elseif ($attributesCount > 1) {
+                        $labels = array_intersect_key($this->generateLabels($tableSchema), array_flip($uniqueColumns));
+                        $lastLabel = array_pop($labels);
+                        $columnsList = implode("', '", $uniqueColumns);
+                        $dynamicModel->addRule($columnsList, 'unique', ['targetAttribute' => [$columnsList], 'message' => 'The combination of " . implode(', ', $labels) . " and " . $lastLabel . " has already been taken.']);
+                    }
+                }
+            }
+        } catch (NotSupportedException $e) {
+            // doesn't support unique indexes information...do nothing
+        }
+        return $dynamicModel;
+    }
+
+    /**
+     * Generates the attribute labels for the specified table.
+     * @param \yii\db\TableSchema $table the table schema
+     * @return array the generated attribute labels (name => label)
+     */
+    protected static function generateLabels($tableSchema)
+    {
+        $labels = [];
+        foreach ($tableSchema->columns as $column) {
+            if ($this->generateLabelsFromComments && !empty($column->comment)) {
+                $labels[$column->name] = $column->comment;
+            } elseif (!strcasecmp($column->name, 'id')) {
+                $labels[$column->name] = 'ID';
+            } else {
+                $label = Inflector::camel2words($column->name);
+                if (!empty($label) && substr_compare($label, ' id', -3, 3, true) === 0) {
+                    $label = substr($label, 0, -3) . ' ID';
+                }
+                $labels[$column->name] = $label;
+            }
+        }
+
+        return $labels;
+    }
+
+}
---
+++ widgets/DynamicForm.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace mata\widgets;
+
+use Yii;
+use yii\helpers\Html;
+use yii\helpers\Json;
+use ReflectionClass;
+
+class DynamicForm extends \matacms\widgets\ActiveForm {
+
+    public $model;
+    public $fieldAttributes = [];
+    public $omitId = true;
+    private $modelAttributes;
+
+	/**
+     * Initializes the widget.
+     * This renders the form open tag.
+     */
+	public function init()
+	{
+		if (!isset($this->options['id'])) {
+			$this->options['id'] = $this->getId();
+		}
+		echo Html::beginForm($this->action, $this->method, $this->options);
+	}
+
+    /**
+     * Runs the widget.
+     * This registers the necessary javascript code and renders the form close tag.
+     * @throws InvalidCallException if `beginField()` and `endField()` calls are not matching
+     */
+    public function run()
+    {
+    	if (!empty($this->_fields)) {
+    		throw new InvalidCallException('Each beginField() should have a matching endField() call.');
+    	}
+        
+        // Set custom attribute labels
+        if(!empty($this->fieldAttributes)) {
+            $attributeLabels = $this->model->getAttributeLabels();
+            foreach($this->fieldAttributes as $fieldName => $fieldAttribute) {
+                if(array_key_exists($fieldName, $attributeLabels) && isset($fieldAttribute['label'])) {
+                    $this->model->setAttributeLabel($fieldName, $fieldAttribute['label']);
+                }
+            }
+        }
+
+        $modelClass = (new ReflectionClass($this->model))->getName();
+
+        $this->modelAttributes = $this->model->attributes;
+
+        // Remove Id from model attributes
+        if($this->omitId) {
+            if(array_key_exists('Id', $this->modelAttributes)) {
+                unset($this->modelAttributes['Id']);
+            }
+        }
+
+        // Generate fields
+        foreach($this->modelAttributes as $fieldName => $fieldValue) {
+            echo $this->generateActiveField($fieldName);
+        }
+    	
+        if ($this->enableClientScript) {
+            $id = $this->options['id'];
+            $options = Json::encode($this->getClientOptions());
+            $attributes = Json::encode($this->attributes);
+            $view = $this->getView();
+            \yii\widgets\ActiveFormAsset::register($view);
+            $view->registerJs("jQuery('#$id').yiiActiveForm($attributes, $options);");
+        }
+
+        echo $this->submitBtns();
+
+        echo Html::endForm();
+    }
+
+    protected function submitBtns() {
+    	echo Html::beginTag('div', ['class'=>'form-group']);
+    	echo Html::submitButton('Create', ['class' => 'btn btn-success']);
+    	echo Html::endTag('div');
+    }
+
+    /**
+     * Generates code for active field
+     * @param string $attribute
+     * @return string
+     */
+    public function generateActiveField($attribute)
+    {
+        if(array_key_exists($attribute, $this->fieldAttributes) && isset($this->fieldAttributes[$attribute]['fieldType'])) {
+            $fieldType = $this->fieldAttributes[$attribute]['fieldType'];
+            $basicField = $this->field($this->model, $attribute);
+            $fieldTypeParams = [];
+            if(is_array($fieldType)) {
+                if(isset($this->fieldAttributes[$attribute]['fieldType'][key($fieldType)]['params'])) {
+                    $fieldTypeParams[] = $this->fieldAttributes[$attribute]['fieldType'][key($fieldType)]['params'];
+                }
+                $fieldType = key($fieldType);
+            }
+            return call_user_func_array(array($basicField, $fieldType), $fieldTypeParams);
+        }
+
+        $tableSchema = $this->model->getInstanceTableSchema();
+        if ($tableSchema === false || !isset($tableSchema->columns[$attribute])) {
+            if (preg_match('/^(password|pass|passwd|passcode)$/i', $attribute)) {
+                return $this->field($this->model, $attribute)->passwordInput();
+            } else {
+                return $this->field($this->model, $attribute);
+            }
+        }
+        $column = $tableSchema->columns[$attribute];
+        if ($column->phpType === 'boolean') {
+            return $this->field($this->model, $attribute)->checkbox();
+        } elseif ($column->type === 'text') {
+            return $this->field($this->model, $attribute)->textarea(['rows' => 6]);
+        } else {
+            if (preg_match('/^(password|pass|passwd|passcode)$/i', $column->name)) {
+                $input = 'passwordInput';
+            } else {
+                $input = 'textInput';
+            }
+            if (is_array($column->enumValues) && count($column->enumValues) > 0) {
+                $dropDownOptions = [];
+                foreach ($column->enumValues as $enumValue) {
+                    $dropDownOptions[$enumValue] = Inflector::humanize($enumValue);
+                }
+                return $this->field($this->model, $attribute)->dropDownList(preg_replace("/\n\s*/", ' ', VarDumper::export($dropDownOptions)), ['prompt' => '']);
+            } elseif ($column->phpType !== 'string' || $column->size === null) {
+                return $this->field($this->model, $attribute)->$input();
+            } else {
+                return $this->field($this->model, $attribute)->$input(['maxlength' => $column->size]);
+            }
+        }
+    }
+    
+}
